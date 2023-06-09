c6df6ea2fcda64470fb3322ecc6a74be2e6ab895
yulia <vaydafox@gmail.com>
2023-06-09 13:46:45 +0300

    Finished. Added method printShapeName.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        System.out.println(new Triangle().printShapeName());
+        System.out.println(new Circle().printShapeName());
+        System.out.println(new Quad().printShapeName());
+        System.out.println(new Rectangle().printShapeName());
+        System.out.println(new Trapezoid().printShapeName());
+}
 }
--- src/Shape.java
+++ src/Shape.java
@@ -2,17 +2,11 @@
 
 public abstract class Shape{
     private String shapeName;
-     public Shape(String shapeName){
-         this.shapeName = shapeName;
-         System.out.println(shapeName);
-     }
-
-    public static void main(String[] args) {
-        new Triangle("Triangle");
-        new Circle("Circle");
-        new Quad("Quad");
-        new Rectangle("Rectangle");
-        new Trapezoid("Trapezoid");
+    public String getShapeName(){
+        return shapeName;
     }
+     public abstract String printShapeName();
+
+
 
 }
